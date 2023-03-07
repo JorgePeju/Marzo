fc9228c84e937dd290672bb7bdec8082df0df399
JorgePeju <pejuco@gmail.com>
2023-03-07 23:00:39 +0100

    cambios de express

--- 7_Marzo_Martes/Express/app.js
+++ 7_Marzo_Martes/Express/app.js
@@ -6,21 +6,31 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 
+
+app.use(express.static('public'));
+// Establecer ejs como template engine
+app.set('view engine' , 'ejs');
+// Estableder cual va a ser la carpeta de vistas
+app.set("views",__dirname + "/views");
 // rutas
+
 app.get('/', (req, res) => {
-    res.send('<h1>INDEX</h1>')
+    res.render('index',{
+        titulo:'Me gustan las cosas' ,
+        parrafo: 'No me gustan las cosas',
+        })
 })
 
 app.get('/productos', (req, res) => {
-    res.send('<h1>Estamos en productos</h1>')
+    res.render({})
 })
 
 app.get('/quienesSomos', (req, res) => {
-    res.send('<h1>Quienes somos?</h1>')
+    res.render('<h1>Quienes somos?</h1>')
 })
 
 app.get('/contacto', (req, res) => {
-    res.send('<h1>Estamos en contacto</h1>')
+    res.render('<h1>Estamos en contacto</h1>')
 })
 
 // Chasqueador
